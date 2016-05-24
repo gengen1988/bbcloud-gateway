d8c46cf82e5d4ae330162640d8f7d1ce874b6b31
Gen Liu <gerald_721@hotmail.com>
2016-05-24 17:22:15 +0800

    add model name

--- routers.js
+++ routers.js
@@ -1,16 +1,16 @@
 var router = require('express').Router();
 var ResourceRouter = require('./lib/resource-router');
 
-router.use(ResourceRouter('administrators'));
+router.use(ResourceRouter('administrators', 'AdministratorAccount'));
 
-router.use(ResourceRouter('roles'));
+router.use(ResourceRouter('roles', 'Role'));
 
-router.use(ResourceRouter('permissions'));
+router.use(ResourceRouter('permissions', 'Permission'));
 
-router.use(ResourceRouter('devices'));
+router.use(ResourceRouter('devices', 'Device'));
 
-router.use(ResourceRouter('customers'));
+router.use(ResourceRouter('customers', 'CustomerAccount'));
 
-router.use(ResourceRouter('manufacturers'));
+router.use(ResourceRouter('manufacturers', 'ManufacturerAccount'));
 
 module.exports = router;
